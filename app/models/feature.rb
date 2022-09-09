00a1f3d6ff21756fe6a39214216e6be498b7b9d5
Dr.HiHi <drhihi@ukr.net>
2022-09-09 04:17:46 +0300

    try to fix feature association

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,5 +1,7 @@
 class Feature < ApplicationRecord
   has_many :feature_values
   # has_many :valuables, through: :feature_values, source: :valuable, as: :valuable
-  has_many :valuables, through: :feature_values, source_type: 'StringValue', as: :valuable
+  # has_many :valuables, through: :feature_values, source_type: 'StringValue', as: :valuable
+  has_many :strs, through: :feature_values, source: :valuable, source_type: 'StringValue'
+  has_many :ints, through: :feature_values, source: :valuable, source_type: 'IntegerValue'
 end
--- app/models/product_feature_value.rb
+++ app/models/product_feature_value.rb
@@ -3,4 +3,6 @@ class ProductFeatureValue < ApplicationRecord
   belongs_to :feature_value
 
   has_many :features, through: :feature_value
+
+  validates_uniqueness_of :product_id, scope: :features
 end
